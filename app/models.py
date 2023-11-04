b6f7ffcc75d46ca40682190a5c661bd07b965b0b
Orrv2904 <Omarrv2904@gmail.com>
2023-11-04 10:43:26 -0600

    :white_check_mark:

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Pizza(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField()
-    image = models.CharField(max_length=500)
+    image = models.CharField(max_length=200)
     
     def __str__(self):
         return self.name
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Pizza
 
-# Create your tests here.
+class PizzaModelTestCase(TestCase):
+    def setUp(self):
+        self.pizza = Pizza.objects.create(
+            name="Margherita",
+            price=10.99,
+            image="margherita.jpg"
+        )
+
+    def test_pizza_name(self):
+        self.assertEqual(self.pizza.name, "Margherita")
+
+    def test_pizza_price(self):
+        self.assertEqual(self.pizza.price, 10.99)
+
+    def test_pizza_image(self):
+        self.assertEqual(self.pizza.image, "margherita.jpg")
+
+    def test_pizza_str_method(self):
+        self.assertEqual(str(self.pizza), "Margherita")
+
+    def test_pizza_model_instance(self):
+        self.assertIsInstance(self.pizza, Pizza)
+
+    def test_pizza_model_fields(self):
+        pizza_fields = Pizza._meta.get_fields()
+        field_names = [field.name for field in pizza_fields]
+        self.assertIn('name', field_names)
+        self.assertIn('price', field_names)
+        self.assertIn('image', field_names)
